23448cf653cc061de5f83f8db39e3bc3f0340e27
leesein-dev <leesein.dev@gmail.com>
2021-10-03 17:01:21 +0900

    rock-scissor-paper mini game ver1 complete

--- src/page/rock-scissor-paper-game/RockScissorPaperGameMain.js
+++ src/page/rock-scissor-paper-game/RockScissorPaperGameMain.js
@@ -1,5 +1,5 @@
-import './RockScissorPaperGameMain..scss';
-import reset from '../../image/rock-scissor-paper/ic-reset.svg';
+import './RockScissorPaperGameMain.scss';
+import resetButton from '../../image/rock-scissor-paper/ic-reset.svg';
 import rock from '../../image/rock-scissor-paper/rock.svg';
 import scissor from '../../image/rock-scissor-paper/scissor.svg';
 import paper from '../../image/rock-scissor-paper/paper.svg';
@@ -34,6 +34,10 @@ function RockScissorPaperGameMain() {
     let [myCount, setMyCount] = useState(0);
     let [otherCount, setOtherCount] = useState(0);
 
+    let [point, setPoint] = useState(1);
+
+    let [record, setRecord] = useState('');
+
     const CHANGE_MY_SELECT = (id, image, description) => {
         setMySelect(image);
         setMyImageDescription(description);
@@ -55,29 +59,76 @@ function RockScissorPaperGameMain() {
         RESULT_COUNT(id, randomNumber);
     }
 
+    let myWin = 'my';
+    let otherWin = 'other';
+    let draw = 'draw';
+    let reset = 'reset';
+
+    function whoWin(value) {
+
+        const WIN_BOX = document.getElementsByClassName("select-box-background");
+        const MY_WIN_BOX = WIN_BOX[0];
+        const OTHER_WIN_BOX = WIN_BOX[1];
+
+        if(value === 'my') {
+            MY_WIN_BOX.classList.add('active')
+            OTHER_WIN_BOX.classList.remove('active')
+        } else if (value === 'other') {
+            OTHER_WIN_BOX.classList.add('active')
+            MY_WIN_BOX.classList.remove('active')
+        } else {
+            OTHER_WIN_BOX.classList.remove('active')
+            MY_WIN_BOX.classList.remove('active')
+        }
+    }
+
+    //문자열 사이 ,(콤마) 넣는 방법으로 추후에 변경하기
     const RESULT_COUNT = (myButton, otherButton) => {
 
+        let myRecord = '나, ';
+        let otherRecord = '상대, ';
+
         if(myButton === otherButton) {
+            whoWin(draw)
             return;
         } else if(myButton === 1 && otherButton === 2) {
-            myCount += 1;
-            return setMyCount(myCount);
+            myCount += (1 * point);
+            record += myRecord;
+            whoWin(myWin);
         } else if (myButton === 2 && otherButton === 3) {
-            myCount += 1;
-            return setMyCount(myCount);
+            myCount += (1 * point);
+            record += myRecord;
+            whoWin(myWin);
         } else if (myButton === 3 && otherButton === 1) {
-            myCount += 1;
+            myCount += (1 * point);
+            record += myRecord;
+            whoWin(myWin);
         } else {
-            otherCount += 1;
+            otherCount += (1 * point);
+            record += otherRecord;
+            whoWin(otherWin);
         }
 
         setMyCount(myCount);
         setOtherCount(otherCount);
+        setRecord(record);
     }
 
     const RESET_COUNT = () => {
+
+        setMySelect(rock);
+        setOtherSelect(rock);
+
         setMyCount(0);
         setOtherCount(0);
+
+        setPoint(1);
+        const POINT_INPUT = document.querySelector('#point');
+        POINT_INPUT.value = 1;
+
+        whoWin(reset);
+
+        setRecord('');
     }
 
     return (
@@ -88,7 +139,7 @@ function RockScissorPaperGameMain() {
                         <span>가위바위보</span>
                     </div>
                     <div id={"last"}>
-                        <img src={reset} alt={"초기화 버튼"} onClick={()=>{RESET_COUNT()}}/>
+                        <img src={resetButton} alt={"초기화 버튼"} onClick={()=>{RESET_COUNT()}}/>
                     </div>
                 </div>
                 <div id={"count-area"}>
@@ -102,19 +153,19 @@ function RockScissorPaperGameMain() {
                 <div id={"result-area"}>
                     <div id={"inner-item"}>
                         <div>
-                            <MySelectBox image={mySelect} description={myImageDescription}/>
+                            <SelectBox image={mySelect} description={myImageDescription}/>
                             <p>vs</p>
-                            <OtherSelectBox image={otherSelect} description={otherImageDescription}/>
+                            <SelectBox image={otherSelect} description={otherImageDescription}/>
                         </div>
                         <div>
                             <span>배점</span>
-                            <input type={"number"} min={1} max={10} defaultValue={"1"}/>
+                            <input id={"point"} type={"number"} min={1} max={10} defaultValue={point} onChange={(e)=>{setPoint(e.target.value)}}/>
                             <span>배</span>
                         </div>
                         <div>
                             <div>
                                 <p>승부 기록</p>
-                                <p>나, 상대, 상대, 나</p>
+                                <p>{record}</p>
                             </div>
                         </div>
                     </div>
@@ -152,17 +203,9 @@ function CountBox(props) {
     )
 }
 
-function MySelectBox({image, description}) {
+function SelectBox({image, description}) {
     return (
-        <div id={"select-box-background"}>
-            <img src={image} alt={description}/>
-        </div>
-    )
-}
-
-function OtherSelectBox({image, description}) {
-    return (
-        <div id={"select-box-background"}>
+        <div className={"select-box-background"}>
             <img src={image} alt={description}/>
         </div>
     )
